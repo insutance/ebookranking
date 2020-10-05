d6224931d0c606b8ed317293a0eeee874b0a5a13
insutance <insutance@gmail.com>
2020-10-05 23:16:33 +0900

    first heroku

--- ebookranking/settings.py
+++ ebookranking/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w&*tq&)&u%e^fseozt7q2it1+(&229q%gx(f2@)$w2sb=$6p+o'
+# SECRET_KEY = 'w&*tq&)&u%e^fseozt7q2it1+(&229q%gx(f2@)$w2sb=$6p+o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
