d775ced5b1a0129137902a2a9b72ea5aecb6dde6
insutance <insutance@gmail.com>
2020-10-12 22:00:50 +0900

    yes24 try except modify

--- index/crawling/crawling_yes24.py
+++ index/crawling/crawling_yes24.py
@@ -22,13 +22,7 @@ def crawling_init():
 
     print('yes24 website start!!')
     driver.get('http://www.yes24.com/24/Category/BestSeller?CategoryNumber=017&sumgb=06&AO=4&FetchSize=50')  # FetchSize 가 한번에 뜨는 책 개수
-    #driver.implicitly_wait(2)  # 버퍼때문에 2초간 기다리게 함
-    try:
-        element = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="category_layout"]/tbody'))
-            )
-    except TimeoutException:
-        return False
+    driver.implicitly_wait(3)  # 버퍼때문에 2초간 기다리게 함
         
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
