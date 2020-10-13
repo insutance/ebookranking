29b89700fa12d13ba0f1d648814fd7d90f6cd5d3
insutance <insutance@gmail.com>
2020-10-13 19:22:36 +0900

    APScheduler test

--- index/clock.py
+++ index/clock.py
@@ -5,17 +5,38 @@ import django
 django.setup()
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from index.crawling import crawling_kyobo
+from index.crawling import crawling_kyobo,crawling_yes24,crawling_aladin,crawling_naver,crawling_ridibooks,crawling_totalbook
 
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=2)
 def crawlied_kyobo():
     crawling_kyobo.kyobo()
-
+    crawling_yes24.yes24()
+    crawling_aladin.aladin()
+    crawling_naver.naver()
+    crawling_ridibooks.ridibooks()
+    crawling_totalbook.total_book()
+"""
 @sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
+def crawlied_yes24():
+    crawling_yes24.yes24()
 
+@sched.scheduled_job('interval', minutes=4)
+def crawlied_aladin():
+    crawling_aladin.aladin()
+
+@sched.scheduled_job('interval', minutes=5)
+def crawlied_naver():
+    crawling_naver.naver()
+
+@sched.scheduled_job('interval', minutes=6)
+def crawlied_ridibooks():
+    crawling_ridibooks.ridibooks()
+
+@sched.scheduled_job('interval', minutes=7)
+def crawlied_totalbook():
+    crawling_totalbook.total_book()
+"""
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
