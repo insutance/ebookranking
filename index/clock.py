e7d68a0612fca209bff39b7d249f289c133342a6
insutance <insutance@gmail.com>
2020-10-14 18:44:47 +0900

    cron background modify

--- index/clock.py
+++ index/clock.py
@@ -5,11 +5,40 @@ import django
 django.setup()
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from index.models import TotalBooks, Book
 from index.crawling import crawling_kyobo,crawling_yes24,crawling_aladin,crawling_naver,crawling_ridibooks,crawling_totalbook
+import time
 
-sched = BlockingScheduler()
+def crawling_job():
+    for i in range(10):
+        while True:
+            try:
+                Book.objects.all().delete()
+                TotalBooks.objects.all().delete()
+                
+                crawling_kyobo.kyobo()
+                crawling_yes24.yes24()
+                crawling_aladin.aladin()
+                crawling_naver.naver()
+                crawling_ridibooks.ridibooks()
+                crawling_totalbook.total_book()
+            except Exception as e:
+                print("Error 발생: " + e)
+                continue
+            break
+        break
 
+#sched = BlockingScheduler()
+scheduler = BackgroundScheduler()
+scheduler.add_job(crawling_job, 'interval', minute=3)
+scheduler.start()
+
+while True:
+    print('time sleep')
+    time.sleep(10)
+
+"""
 @sched.scheduled_job('cron', hour='*')
 def scheduled_job():
     for i in range(10):
@@ -31,3 +60,4 @@ def scheduled_job():
         break
 
 sched.start()
+"""
