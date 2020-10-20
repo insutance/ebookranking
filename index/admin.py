8cd6010ef02ad9a68a7ebc8fc3007a4384f617cd
insutance <insutance@gmail.com>
2020-10-20 22:02:49 +0900

    admin 커스터마이징

--- index/admin.py
+++ index/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Book, TotalBooks
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['rank', 'title', 'price', 'bookstore', 'weight', 'date']
+    list_display_links = ['title']
+
+class TotalBookAdmin(admin.ModelAdmin):
+    list_display = ['rank', 'title', 'weight', 'date']
+    list_display_links = ['title']
+
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(TotalBooks)
+admin.site.register(Book, BookAdmin)
+admin.site.register(TotalBooks, TotalBookAdmin)
 
